8d6b520d58703d93eceec0da53725109c5d84201
Van Huynh <vthuynh@cpp.edu>
2022-09-20 11:02:07 -0700

    implement basic student class

--- Project1-cs3560/src/main/java/SimulationDriver.java
+++ Project1-cs3560/src/main/java/SimulationDriver.java
@@ -1,5 +1,11 @@
 public class SimulationDriver {
     public static void main (String[] arg) {
+        /**
+         * 1. generate random questions
+         * 2. generate random number of students
+         * 3. generate random answers from students (must be greater than student amount)
+         * 4. call voting services
+         * 5. Display output/statistic for each question asked*/
         System.out.println("Hello World");
     }
 }
